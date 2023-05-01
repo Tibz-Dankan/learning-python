d7e14882dc710c1713ad5674200a5e18fc764e8d
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-02 02:27:23 +0300

    imports in python

---
+++ imports.py
@@ -0,0 +1,21 @@
+# Example 1
+import random
+
+for i in range(5):
+    value = random.randint(100,800)  # integer value in the range of 100 and 800
+    print(value)
+
+# Example 2
+from math import pi
+print(pi)
+
+# Example 3
+from math import  pi, sqrt
+
+# Example 4 
+#import some_module #Gives an importError
+
+
+# Example 5
+from math import sqrt as square_root
+print(square_root(100))
