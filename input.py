01eb649b3265592715f4947fcfcf72d4b6ed3097
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-02 00:07:00 +0300

    get user input in python

---
+++ input.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+
+print("Hello "+ name + ", nice to you learning python!")
+
+
+age = input("Enter your age: ")
+
+print("You have "+ age +" years !")
+
+
+year_of_birth = input("Enter your year of birth: ")
+print("You are "+ str(2023-int(year_of_birth))+ " years old")
+
+# Note
+ # str() converts to string and 
+ # int() converts to an integer
+# float() converts to float
+
