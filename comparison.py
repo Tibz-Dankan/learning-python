931a928efb5a0e86733f45d9f318c6d18455cbc9
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-02 00:49:13 +0300

    boolean logic in python

--- comparison.py
+++ comparison.py
@@ -13,3 +13,22 @@ print("Anni" !="Andy")  #prints True
 # Greater than or equal (>=) | Less than or equal (<=)
 print(8.0 >= 8)  #print True
 
+# Boolean logic 
+# Used to make to more complicated conditions for if statements that reply on more than one condition
+
+# Boolean operator (and)
+print(1==1 and 2==2)  #prints True
+print(1!=1 and 2==2)  #prints false
+print(2>1 and 3>6)   #prints false
+
+# Boolean operator (or)
+print(1==1 or 2==2)  #prints True
+print(1==1 or 2==3)  #prints True
+print(2>1 or 3>6)   #prints false
+
+# Boolean operator (Not), not takes on argument abd inverts
+print(not 1==1)
+print(not 1>7)
+
+# operator precedence
+# parenthesis --> exponentiation --> multiplication/division --> addition/subtraction
