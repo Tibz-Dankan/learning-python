ab1313b642d28254ffdc4c17fcb58b19c7d1fec4
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-01 23:54:42 +0300

    very basics  of python

--- basics.py
+++ basics.py
@@ -1,3 +1,36 @@
 # Hello world statement
-
 print("Hello Word")
+
+# printing text
+print("Dankan is learning python")
+print(2002)
+print(20+530)
+
+# simple operations
+print(5+9)
+print(5-9)
+print(9%5)
+print(9/9)
+
+# Floats
+print(0.25)
+print(4/0.5)
+print(10.4/2)
+
+# exponentiation
+print(2**5)
+print(2**5**3)
+
+
+# strings
+print("This is  a Double string")
+print('This is  a single string')
+print("This is "+" a cat")  # string concatenation
+print("Dankan "* 3)   # prints the string 3 times
+
+
+# variables
+name = "Dankan"
+print("Dankan")
+x= 57
+print(5+7)
