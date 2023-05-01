1f767a447f85a45787b5b5d4879369fefb179d7e
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-02 00:15:30 +0300

    comparison operator in python

---
+++ comparison.py
@@ -0,0 +1,15 @@
+# Boolean, Comparison operator
+
+# Either True or False
+print(3.0 == 3) # prints True
+print(3> 100) # prints False
+print("Dankan" == "Tibz") # prints False
+
+
+# Not Equal operator
+print(4 != 4) # prints False
+print("Anni" !="Andy")  #prints True
+
+# Greater than or equal (>=) | Less than or equal (<=)
+print(8.0 >= 8)  #print True
+
